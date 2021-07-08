7847ee9d26e2412387c299629ea22e3b01642437
Nekai <62569931+Belotti01@users.noreply.github.com>
2021-07-08 19:08:34 +0200

    Add extension method IEnumerable<T>.RemoveDefaults().

---
+++ Extensions/Enumerables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NL.Extensions {
+    public static class Enumerables {
+
+        public static IEnumerable<T> RemoveDefaults<T>(this IEnumerable<T> collection) {
+            return collection
+                .Where(item => !item.IsDefault());
+        }
+
+    }
+}
