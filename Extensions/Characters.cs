a4a0d01052e4a2e94984e052f26219dbec11883a
Nekai <62569931+Belotti01@users.noreply.github.com>
2021-08-16 17:22:49 +0200

    Add extension methods for char.ToLower() and char.ToUpper().

--- Extensions/Characters.cs
+++ Extensions/Characters.cs
@@ -4,6 +4,18 @@ using System.Text;
 
 namespace NL.Extensions {
     public static class Characters {
-        
+
+        public static char ToUpper(this char character) {
+            return char.IsLetter(character)
+                ? char.ToUpper(character)
+                : character;
+        }
+
+        public static char ToLower(this char character) {
+            return char.IsLetter(character)
+                ? char.ToLower(character)
+                : character;
+        }
+
     }
 }
