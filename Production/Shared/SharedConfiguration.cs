2213c8ced68730074e5edb1797995a0f862397b7
Nekai <62569931+Belotti01@users.noreply.github.com>
2021-08-29 23:06:55 +0200

    Add methods to handle settings and configs shared between multiple programs of the same ecosystem.

---
+++ Production/Shared/SharedConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using NL.Serialization;
+
+namespace NL.Production.Shared {
+
+    public class SharedConfiguration {
+        [JsonIgnore]
+        private string SharedSettingsFilePath => SharedFolder.SharedSettingsFilePath;
+
+        #region Settings
+        [JsonProperty("Username")]
+        public string Username { get; set; } = "Guest";
+        #endregion
+
+        public void Save() {
+            Json.Serialize(this, SharedSettingsFilePath, 10000);
+        }
+    }
+
+}
---
+++ Production/SharedFolder.cs
@@ -0,0 +1,36 @@
+﻿using NL.Production.Shared;
+using NL.Serialization;
+using System.IO;
+using System.Text;
+
+namespace NL.Production {
+    public static partial class SharedFolder {
+
+        private const string SHARED_SETTINGS_FILENAME = "CheapHouse_Config.json";
+        /// <summary>
+        ///     The full path to the folder storing the programs' settings and data.
+        /// </summary>
+        public static readonly string SharedSettingsFolder;
+        /// <summary>
+        ///     The full path to the shared configuration's json file.
+        /// </summary>
+        public static readonly string SharedSettingsFilePath;
+        /// <summary>
+        ///     The configuration shared between programs.
+        /// </summary>
+        public static readonly SharedConfiguration Configuration;
+
+        static SharedFolder() {
+            // Initialize filepaths and create missing folders & files
+            string root = Path.GetPathRoot(Directory.GetCurrentDirectory());
+            SharedSettingsFolder = Path.Combine(root, "CheapHouse");
+
+            Directory.CreateDirectory(SharedSettingsFolder);
+            SharedSettingsFilePath = Path.Combine(SharedSettingsFolder, SHARED_SETTINGS_FILENAME);
+            if(!Json.TryDeserialize(SharedSettingsFilePath, out Configuration)) {
+                Configuration = new SharedConfiguration();
+                Json.Serialize(Configuration, SharedSettingsFilePath);
+            }
+        }
+    }
+}
