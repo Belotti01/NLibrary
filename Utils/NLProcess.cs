66641c0d0b1a21bdd37232512b8d4fbbba215d75
Nekai <62569931+Belotti01@users.noreply.github.com>
2021-08-29 23:07:46 +0200

    Porting of the VB equivalent of the Shell() method.

---
+++ Utils/NLProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NL.Utils {
+    public static class NLProcess {
+
+        public static Process StartShellProcess(string filepath, string arguments, bool showWindow = true) {
+            ProcessStartInfo processInfo = new() {
+                FileName = filepath,
+                Arguments = arguments,
+                UseShellExecute = true,
+                WindowStyle = showWindow
+                    ? ProcessWindowStyle.Normal
+                    : ProcessWindowStyle.Hidden
+            };
+
+            return Process.Start(processInfo);
+        }
+
+        public static Process StartShellProcess(string filepath, bool showWindow = true)
+            => StartShellProcess(filepath, string.Empty, showWindow);
+
+    }
+}
